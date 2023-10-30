a793380adb6d532db93acd47123af5863529969f
Idriss Dassi Kamdoum <87118993+prims06@users.noreply.github.com>
2023-10-30 15:58:12 +0100

    wallet getted

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -2,40 +2,55 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ResponseDeposit, ResponseTransactionList } from '../models/Transaction';
+import {
+  ResponseDeposit,
+  ResponseTransactionList,
+} from '../models/Transaction';
 import { ResponseProfile } from '../models/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TransactionService {
+  private urlDeposit = environment.backend_api_url + environment.url_deposit;
+  private urlUser = environment.backend_api_url + environment.user_url;
+  private urlTransactionList =
+    environment.backend_api_url + environment.url_transaction_list;
+  private tokenRegistred: any = localStorage.getItem('token-mansexch');
+  private data: any = JSON.parse(this.tokenRegistred);
 
-  private urlDeposit = environment.backend_api_url + environment.url_deposit
-  private urlUser = environment.backend_api_url + environment.user_url
-  private urlTransactionList = environment.backend_api_url + environment.url_transaction_list
-  private tokenRegistred: any = localStorage.getItem('token-mansexch')
-  private data: any = JSON.parse(this.tokenRegistred)
-  
   private config = {
-    headers: new HttpHeaders(
-      {
-        "Content-Type": "application/json",
-        'Authorization': `Bearer ${this.data.token}`
-      }
-    )
-  }
-  
-  constructor(private http: HttpClient) { }
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.data.token}`,
+    }),
+  };
+
+  constructor(private http: HttpClient) {}
 
   getProfile(): Observable<ResponseProfile> {
-    return this.http.get<ResponseProfile>(`${this.urlUser}/profile`, this.config)
+    return this.http.get<ResponseProfile>(
+      `${this.urlUser}/profile`,
+      this.config
+    );
   }
-  
+
   addDeposit(data: any): Observable<ResponseDeposit> {
-    return this.http.post<ResponseDeposit>(`${this.urlDeposit}/deposits`, data, this.config)
+    return this.http.post<ResponseDeposit>(
+      `${this.urlDeposit}/deposits`,
+      data,
+      this.config
+    );
   }
 
   getAllTransaction(): Observable<ResponseTransactionList> {
-    return this.http.get<ResponseTransactionList>(`${this.urlTransactionList}/all`, this.config)
+    try {
+      return this.http.get<ResponseTransactionList>(
+        `${this.urlTransactionList}/all`,
+        this.config
+      );
+    } catch (error) {
+      throw new Error();
+    }
   }
 }
--- src/app/shared/components/crypto-balance/crypto-balance.component.ts
+++ src/app/shared/components/crypto-balance/crypto-balance.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataCryptoService } from 'src/app/services/data-crypto.service';
 
 @Component({
   selector: 'app-crypto-balance',
   templateUrl: './crypto-balance.component.html',
-  styleUrls: ['./crypto-balance.component.scss']
+  styleUrls: ['./crypto-balance.component.scss'],
 })
-export class CryptoBalanceComponent {
+export class CryptoBalanceComponent implements OnInit {
+  wallet: any[];
+  response:any;
+  constructor(private cryptoService: DataCryptoService) {}
 
+  ngOnInit(): void {this.getWalletDetails()}
+
+  getWalletDetails() {
+    this.cryptoService.getWalletDetails().subscribe((value) => {
+      this.response = value;
+      if(this.response && this.response.statusCode==1000){
+        this.wallet = this.response.data.details.filter((e:any)=>e.details.type);
+      }
+    });
+  }
 }
