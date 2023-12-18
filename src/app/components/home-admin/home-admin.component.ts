a7de1a8a31b991200325d1960bf0264751a4d593
Idriss Dassi Kamdoum <87118993+prims06@users.noreply.github.com>
2023-12-18 19:45:07 +0100

    after create admin service

--- src/app/components/home-admin/home-admin.component.ts
+++ src/app/components/home-admin/home-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as chartData from '../../shared/data/chartData'
 import { UserService } from 'src/app/services/user.service';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-home-admin',
@@ -11,45 +12,18 @@ export class HomeAdminComponent implements OnInit {
   public growthChart = chartData.growthChart;
   private users: any
   private solde: any
-  //private transaction: any
-  //public barChart = chartData.barChart
-
-  /*constructor(private userService: UserService) {}
-
-  ngOnInit(): void {
-    this.userService.getUsersStatistics('all').subscribe((res: any) => {
-      this.users = res.data.users
-      this.growthChart.series = [this.users.total_users, this.users.connected_users, this.users.unconnected_users]
-      this.solde = res.data.wallets
-      this.transaction = res.data.transactions
-      console.log(this.transaction)
-      this.barChart.series[0].data = [
-        this.solde.XAF_balance, 
-        this.solde.BTC_balance, 
-        this.solde.ETH_balance,
-        this.transaction.deposit_transactions_amount,
-        this.transaction.withdraw_transactions_amount,
-        this.transaction.crypto_recharge_transactions_amount,
-        this.transaction.crypto_withdraw_transactions_amount
-      ]
-    })
-  }
-}*/
-
   public datas:any
   private transaction: any
   public barChart = chartData.barChart
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private adminService:AdminService) {}
 
   ngOnInit(): void {
     this.fetchStatistics('all')
   }
   fetchStatistics(country:string){
-    console.log(country)
     this.userService.getUsersStatistics(country).subscribe((res: any) => {
       this.datas = res.data
-      console.log(this.datas)
       this.users = res.data.users
       this.growthChart.series = [this.users.total_users, this.users.connected_users, this.users.unconnected_users]
       this.solde = res.data.wallets
---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseParent } from '../models/Transaction';
+
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AdminService {
+    private urlDeposit = environment.backend_api_url + environment.url_deposit;
+    private urlAdmin = environment.backend_api_url + environment.admin_url;
+
+    private tokenRegistred: any = localStorage.getItem('token-mansexch');
+    private data: any = JSON.parse(this.tokenRegistred);
+
+    private config = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${this.data.token}`,
+        }),
+    };
+
+    constructor(private http: HttpClient) { }
+
+    getConfigs(): Observable<ResponseParent> {
+        return this.http.get<ResponseParent>(
+            `${this.urlAdmin}/configs`,
+            this.config
+        ).pipe(catchError((error) => {
+            return of(error.error)
+        }));
+    }
+
+    setConfigs(body: any): Observable<ResponseParent> {
+        return this.http.put<ResponseParent>(
+            `${this.urlAdmin}/configs`, body,
+            this.config
+        ).pipe(catchError((error) => {
+            return of(error.error)
+        }));
+    }
+
+    banAnUser(uid: string, action: "active" | "banned" | "suspended"): Observable<ResponseParent> {
+        return this.http.put<ResponseParent>(
+            `${this.urlAdmin}/ban?userId=${uid}&action=${action}`,
+            this.config
+        ).pipe(catchError((error) => {
+            return of(error.error)
+        }));
+    }
+
+    kyc(uid: string, action: "approved" | "rejected", document_type: string): Observable<ResponseParent> {
+        return this.http.put<ResponseParent>(
+            `${this.urlAdmin}/kyc?userId=${uid}&action=${action}&document_type=${document_type}`,
+            this.config
+        ).pipe(catchError((error) => {
+            return of(error.error)
+        }));
+    }
+
+
+}
