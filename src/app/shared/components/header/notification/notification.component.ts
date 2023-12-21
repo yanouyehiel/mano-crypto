2dea0e5be477e4b6b3330f57c3ff0c524959f26e
Idriss Dassi Kamdoum <87118993+prims06@users.noreply.github.com>
2023-12-21 22:05:42 +0100

    notifications kyc

--- src/app/shared/components/header/notification/notification.component.ts
+++ src/app/shared/components/header/notification/notification.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss']
 })
-export class NotificationComponent {
+export class NotificationComponent implements OnInit{
+  public user:any
+  public notifications:any[] = []
 
   constructor() {}
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user-mansexch')!).user
+    this.buildNotifis()
+  }
+
+  buildNotifis(){
+    (this.user.kyc as any[]).forEach((item)=>{
+      if(item.document_type=='cni'){
+        this.notifications.push({
+          status:item.status,
+          title:(item.status=='rejected')?'Rejeté':(item.status=='approved')?'Validé':(item.status=='submitted')?'En attente':'Requis',
+          text:(item.status=='rejected')?'Votre piece d\'identité a été rejeté, Veuillez la ressoumettre !':(item.status=='approved')?'Votre piece d\'identité a été approuvé !':(item.status=='submitted')?'Votre piece d\'identité est en cours de validation !':'Veuillez soumettre votre piece d\'identité pour faire valider votre compte !',
+          type:(item.status=='rejected')?'danger':(item.status=='approved')?'success':(item.status=='submitted')?'info':'warning'
+        })
+      }else{
+        this.notifications.push({
+          status:item.status,
+          title:(item.status=='rejected')?'Rejeté':(item.status=='approved')?'Validé':(item.status=='submitted')?'En attente':'Requis',
+          text:(item.status=='rejected')?'Votre selfie a été rejeté, Veuillez le ressoumettre !':(item.status=='approved')?'Votre selfie a été approuvé !':(item.status=='submitted')?'Votre selfie est en cours de validation !':'Veuillez soumettre votre selfie pour faire valider votre compte !',
+          type:(item.status=='rejected')?'danger':(item.status=='approved')?'success':(item.status=='submitted')?'info':'warning'
+        })
+      }
+      
+    })
+  }
 }
