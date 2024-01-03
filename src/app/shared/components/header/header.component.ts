5d7ad6783fb6f9a010356b35224c6793ace4e031
yanouyehiel <yanou.yehiel@yahoo.com>
2024-01-03 12:01:47 +0100

    Resolution du probleme dans le retrait de fonds

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ResponseUser } from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth.service';
 import { NavService } from 'src/app/services/nav.service';
 
@@ -11,7 +13,11 @@ import { NavService } from 'src/app/services/nav.service';
 export class HeaderComponent implements OnInit {
 
   collapseSidebar: boolean = true;
-  constructor(private authService: AuthService, private router: Router, private navServices: NavService) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+    private navServices: NavService,
+    private toast: ToastrService) {}
   open = false;
 
   ngOnInit(): void {
@@ -30,11 +36,19 @@ export class HeaderComponent implements OnInit {
     this.navServices.collapseSidebar = !this.navServices.collapseSidebar;
   }
   
-   logout() {
-    this.authService.logout().subscribe((res: any) => {
-      localStorage.removeItem('user-mansexch')
-      localStorage.removeItem('token-mansexch')
-      this.router.navigate(['/auth/login'])
+  logout() {
+    this.authService.logout().subscribe((res: ResponseUser) => {
+      if (res.statusCode === 1000) {
+        localStorage.removeItem('user-mansexch')
+        localStorage.removeItem('token-mansexch')
+        this.router.navigate(['/auth/login'])
+      }   
+    }, (error) => {
+      if (error.error.statusCode === 1001) {
+        this.toast.error('Veuillez réessayer plus tard !')
+      } else if (error.error.statusCode === 1005) {
+        this.toast.error("Vous n'êtes pas authorisé.")
+      }
     });
   }
 }
